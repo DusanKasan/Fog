68a34d814927ef6f3085441e17a99ffe0ec90227
Dusan Kasan <dusan@kasan.sk>
2014-10-15 15:11:02 +0200

    Adding dummy default class

--- src/Fog/HttpMethod.php
+++ src/Fog/HttpMethod.php
@@ -6,8 +6,21 @@ use Garoevans\PhpEnum\Reflection\Enum;
 
 class HttpMethod extends Enum
 {
+    /**
+     * A dummy default, value since constructor parameter is required.
+     */
+    const __default = self::GET;
+
     const PUT = 'PUT';
     const DELETE = 'DELETE';
     const POST = 'POST';
     const GET = 'GET';
+
+    /**
+     * @param string $enum constant of this class
+     */
+    public function __construct($enum)
+    {
+        parent::__construct($enum, TRUE);
+    }
 }
